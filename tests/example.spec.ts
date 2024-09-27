107e67883139b7e3e3482bcca46145902f677f3f
Max Barannyk <maxrexfax@yandex.ua>
2024-09-27 14:58:49 +0300

    Second commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/docs/intro');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page).toHaveTitle(/Installation/);
+  await page.mouse.wheel(0, 1000);
+  await page.waitForTimeout(1000);
+  await page.mouse.wheel(0, 1000);
+  await page.waitForTimeout(1000);
+  await page.mouse.wheel(0, 1000);
+  await page.waitForTimeout(1000);
+  await page.mouse.wheel(0, 1000);
+  await page.pause();
 });
